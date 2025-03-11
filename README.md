b7ea6a49f7558cfb2f2623ec07cbc074ecb052e6
UmeshG8000 <smitajambukiya@gmail.com>
2025-03-11 20:39:37 +0530

    Code

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Kotlin_Fitness_App
+# Fitness-App
+This is the Android App for track the running progress & statistics
+______________________________________________________________________________________________________________________________________________________________________________
+
+App Features:
+
+1) User select the start destination and start running and it is shown in Google map updated location as user running
+2) Also app shown notification to user about the current running progress and can pause and resume the running progress
+3) Once the running completed saved in the database(room) alongwith running statistics like below
+4) Total distance of running, Total time of running, Average Speed of the user, Total calories burned during the one running session
+5) User can sort the pervious running statistics based on Total time, Average speed, Calories Burned & Total distance of running
+6) Can edit the weight as user running progress increases
+
+______________________________________________________________________________________________________________________________________________________________________________
+
+Tech Used:
+1) Navigation component 
+2) Google-map API
+3) Foreground Service
+4) Recycler-View
+5) MVVM archicture
+6) Prefence datastore
+7) Dagger-hilt
+8) MP-android chart
+9) Room-database
+10) LiveData
+11) Navigation safe-args
+12) Runtime permission's
+
+
+______________________________________________________________________________________________________________________________________________________________________________
+
+Map View, User Notifications, Permissions Requests and User Statistics Screens
+______________________________________________________________________________________________________________________________________________________________________________
+
+![one](https://user-images.githubusercontent.com/90719979/153741453-169bfb4c-309a-4615-9a68-6edd0a2bac6b.jpg)
+
+______________________________________________________________________________________________________________________________________________________________________________
+
+User Login, All Runs in RecyclerView, Settings Screens
+______________________________________________________________________________________________________________________________________________________________________________
+
+![two](https://user-images.githubusercontent.com/90719979/153741467-d5a09b9d-9859-43bc-8f7c-4765907f207c.jpg)
+
+______________________________________________________________________________________________________________________________________________________________________________
---
+++ app/src/main/java/com/example/fitnessapp/ui/fragment/RunFragment.kt
@@ -0,0 +1,235 @@
+package com.example.fitnessapp.ui.fragment
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fitnessapp.R
+import com.example.fitnessapp.adapter.RunAdapter
+import com.example.fitnessapp.other.Constants.MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION
+import com.example.fitnessapp.other.Constants.MY_PERMISSIONS_REQUEST_LOCATION
+import com.example.fitnessapp.other.SortType
+import com.example.fitnessapp.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
+
+
+@AndroidEntryPoint
+class RunFragment : Fragment(R.layout.fragment_run) {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var runAdapter: RunAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkLocationPermission()
+        setUpRecyclerView()
+
+        val workTypeArray: Array<String> = resources.getStringArray(R.array.filter_options)
+        val arrayAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(requireActivity(), R.layout.dropdown_menu, workTypeArray)
+        spFilter.setAdapter(arrayAdapter)
+
+        when (viewModel.sortType) {
+            SortType.DATE -> "DATE"
+            SortType.RUNNING_TIME -> "RUNNING_TIME"
+            SortType.DISTANCE -> "DISTANCE"
+            SortType.AVG_SPEED -> "AVG SPEED"
+            SortType.CALORIES_BURNED -> "CALORIES_BURNED"
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                when (position) {
+                    0 -> viewModel.sortRun(SortType.DATE)
+                    1 -> viewModel.sortRun(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRun(SortType.DISTANCE)
+                    3 -> viewModel.sortRun(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRun(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_fragmentRunTracking)
+        }
+    }
+
+    private fun setUpRecyclerView() = rvRuns.apply {
+        runAdapter = RunAdapter()
+        adapter = runAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(requireActivity())
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, _ ->
+                        //Prompt the user once explanation has been shown
+                        requestLocationPermission()
+                    }
+                    .create()
+                    .show()
+            } else {
+                // No explanation needed, we can request the permission.
+                requestLocationPermission()
+            }
+        } else {
+            checkBackgroundLocation()
+        }
+    }
+
+    private fun checkBackgroundLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestBackgroundLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            MY_PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+
+    private fun requestBackgroundLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ),
+                MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(
+                            requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // Now check background location
+                        checkBackgroundLocation()
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(requireActivity(), "permission denied", Toast.LENGTH_LONG).show()
+
+                    // Check if we are in a state where the user has denied the permission and
+                    // selected Don't ask again
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        startActivity(
+                            Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts("package", requireActivity().packageName, null),
+                            ),
+                        )
+                    }
+                }
+                return
+            }
+            MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(
+                            requireActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(
+                            requireActivity(),
+                            "Granted Background Location Permission",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(requireActivity(), "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+}
